3f67ec1b1188a7e6588961bdef949c3a73423d6c
Nick Palenchar <nickpal@nickpalenchar.com>
2023-09-21 12:43:52 -0400

    wip draw wallx

--- snake/snake.py
+++ snake/snake.py
@@ -1,6 +1,7 @@
 import curses
 from time import sleep
 
+WALL = "#"
 
 class Screen:
     def __init__(self, stdscr, w, h):
@@ -8,25 +9,29 @@ class Screen:
         self.w = w
         self.h = h
 
-    def refresh(self, points):
-        screen = [["·" for _ in range(self.w)] for _ in range(self.h)]
+    def refresh(self, points, strict=False):
+
+        for y, x, char in self.get_border_points():
+            self._stdscr.addstr(y, x, char)
+        for y, x, char in points:
+            self._stdscr.addstr(y, x, char[0])
+        
+        self._stdscr.refresh()
+
+    def get_border_points(self):
+        
+        #return  [(40, y, WALL) for y in range(self.h)] #+ [(y, self.w-1, WALL) for y in range(self.h)]
+        return [(0, x, WALL) for x in range(self.w)] + [(y, 0, WALL) for y in range(self.h)] + [(y, self.w-1, WALL) for y in range(self.h)] + [(self.h-1, x, WALL) for x in range(self.w)]
+
 
 def main(stdscr):
     # init functions
     curses.noecho()
     stdscr.keypad(True)
-    
-    # add string hello, call refresh to actually display
-    stdscr.addstr('hello')
-    stdscr.refresh()
-    sleep(2)
 
-    # erase hello and then add a new string, call
-    # refresh to update the screen. The resulting
-    # effect is hello is replaced by goodbye
-    stdscr.erase()
-    stdscr.addstr('goodbye')
-    stdscr.refresh()
+    screen = Screen(stdscr, 40, 20)
+
+    screen.refresh([(0,0, 'X')])
     sleep(2)
 
 if __name__ == '__main__':
